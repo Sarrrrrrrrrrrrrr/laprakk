88b86452bc3e4ea7922120d095332a72d45cae92
Sarrrrrrrrrrrrrr <156974529+Sarrrrrrrrrrrrrr@users.noreply.github.com>
2024-11-06 19:11:23 +0700

    cotsal.go

--- Tugasmodul_2/Cotsal_2/cotsal.go
+++ Tugasmodul_2/Cotsal_2/cotsal.go
@@ -2,18 +2,16 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
-	var r int
+	var r float64
 
 	fmt.Print("Jejari = ")
 	fmt.Scan(&r)
 	pi := 3.1415926535
+	v := (4.0 / 3.0) * pi * (r * r * r)
+	l := 4 * pi * (r * r)
 
-	v := (4.0 / 3.0) * pi * math.Pow(float64(r), 3)
-	l := 4 * pi * math.Pow(float64(r), 2)
-
-	fmt.Printf("Bola dengan jari-jari %d memiliki volume %.10f dan luas permukaan %.10f\n", r, v, l)
+	fmt.Println("Bola dengan jari-jari", r, "memiliki volume", v, "dan luas permukaan", l)
 }
